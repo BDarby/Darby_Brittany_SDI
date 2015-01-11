e62189eae803cb4ca751b414daedc4a74ccdf001
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-10 20:17:29 -0500

    This is long over due and oops
    set up basic files and started coding.

---
+++ Day 3/js/script.js
@@ -0,0 +1,104 @@
+/*
+Brittany Darby
+1/10/2015
+Day 3 Coding
+ */
+
+// Single lined comment
+
+//This is an alert
+//alert("Testing to see if this is connected");
+
+//Console.log - allows programmers to see information
+console.log("This is the console.");
+
+
+//Declare a variable
+// use the keyword of var
+var whatever1;    //Declaration of a variable
+whatever1 = 42;   // Definition of a variable
+
+console.log(whatever1);
+
+// Declare and define a variable
+var a = 2;
+console.log(a);
+
+//Simple math
+ a = a+3;
+console.log(a);
+
+var b;
+b= a+3;
+console.log(b);
+console.log(a);
+
+//Find our age
+var yearBorn = 1993;
+
+//age is calculated by current year minus year born
+var currentYear = 2015
+
+var age = currentYear - yearBorn - 1;
+console.log(age);
+
+//Math
+//+, -, *, /,
+
+//find area of triangle
+// 1/2 * base *height
+var base = 8;
+var height = 10;
+
+var areaTrangle = 1/2 * base * height;
+console.log(areaTrangle);
+
+// modulo - %
+//Gives remainder
+var remainder = 32%10
+console.log (remainder);
+
+//Find if even or odd
+var evenOrgOdd = 67%2;
+console.log(evenOrgOdd);
+
+
+
+
+//Assignment Operators
+/*
+= Assignment
+ ++ Adds 1 to the variable
+ -- subtracts from the variable
++-  Addition assignment
++= number addtion assignment
+-= number minus assignment
+/= number division assigment
+*= number mutiply assignment
+ */
+
+var counter = 1 ;
+counter++;   //counter = counter 1;
+console.log(counter);
+
+counter--; //counter + counter - 1;
+console.log(counter);
+
+counter +=3;  //counter = counter + 3;
+console.log (counter);
+
+counter -= 2;  // counter = counter -2
+console.log (counter);
+
+counter /= 2;  //counter = counter /2
+console.log(counter);
+
+counter *=4;  //counter = counter *4
+console.log(counter);
+
+
+
+
+
+
+
