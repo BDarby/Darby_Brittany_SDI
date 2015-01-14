9e8e3b3810d5749032d63a4c79063f4d00c1677a
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-13 23:36:30 -0500

    Darby_Brittany_Expressions_Assignment
    added a new prompt.

--- Day 3/Day3LabExpressions/js/Day4/Darby_Brittany_Expressions_Assignment/js/Darby_Brittany_Expressions_Assignment.js
+++ Day 3/Day3LabExpressions/js/Day4/Darby_Brittany_Expressions_Assignment/js/Darby_Brittany_Expressions_Assignment.js
@@ -13,7 +13,13 @@ var rectangle= [6,4,9];  //this is my array of numbers you can choose from. I ch
 var length= parseInt(prompt("Please enter one number 6,4,9 for the length"));
 var width= parseInt(prompt("Please enter one number 6 4,9 for the width"));
 
+
 var total= (2* (length + width)); // this is the formula to find the perimeter of a rectangle
 
 console.log("The length of the rectangle is " +length+ " and the width of the rectangle is " +width+ " thus the total perimeter of the rectangle is " +total);
 // above is my concatenation and answer.
+
+var numberAdd= parseInt(prompt("Pick a number 1-10 to add on the rectangle for fun!")); // this is my 3rd prompt.
+
+total2= (2*(length+width))+numberAdd; //the new equation for the 3rd prompt
+console.log("You added the number " +numberAdd+ " so the new total is " +total2);// new concatenation for the 3rd prompt
