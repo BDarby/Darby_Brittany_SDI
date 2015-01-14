5ae6803c9ffa3f02eb0032d90ae32b11c6289c81
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-13 23:00:39 -0500

    Darby_Brittany_Expression_Assignment
    This is what I have so far on my code.

---
+++ Day 3/Day3LabExpressions/js/Day4/Darby_Brittany_Expressions_Assignment/js/Darby_Brittany_Expressions_Assignment.js
@@ -0,0 +1,19 @@
+/*
+Brittany Darby
+SDI
+1/13/2015
+ */
+var alert=("Testing to see if this works"); //this is the alert to see if everything is working
+
+
+// Finding the Perimeter of a rectangle
+
+var rectangle= [6,4,9];  //this is my array of numbers you can choose from. I choose rectangle
+
+var length= parseInt(prompt("Please enter one number 6,4,9 for the length"));
+var width= parseInt(prompt("Please enter one number 6 4,9 for the width"));
+
+var total= (2* (length * width)); // this is the formula to find the perimeter of a rectangle
+
+console.log("The length of the rectangle is " +length+ " and the width of the rectangle is " +width+ " thus the total perimeter of the rectangle is " +total);
+// above is my concatenation and answer.
