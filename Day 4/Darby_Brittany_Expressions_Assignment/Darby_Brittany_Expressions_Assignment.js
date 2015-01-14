e04b18ad497b0af99092e76f2417e3094df782ad
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-14 13:22:31 -0500

    Darby_Brittany_Expressions_Assignment
    I have made more drastic changes to my flowchart and I still can’t
    understand what I am doing wrong on my math formula. It’s the right
    formula, but in the end it, in concatenation it says NaN.

--- Day 4/Darby_Brittany_Expressions_Assignment/Darby_Brittany_Expressions_Assignment.js
+++ Day 4/Darby_Brittany_Expressions_Assignment/Darby_Brittany_Expressions_Assignment.js
@@ -3,7 +3,7 @@ Brittany Darby
 SDI sec.1 C201501
 1/13/2015
  */
-var alert=("Testing to see if this works"); //this is the alert to see if everything is working
+ //alert=("Testing to see if this works"); //this is the alert to see if everything is working
 
 
 // Finding the Area of a Trapezoid
@@ -16,7 +16,7 @@ var height= parseInt(prompt("Please enter a number for the height"));
 var trapezoid= [base1,base2,height];//this is my array for the trapezoid
 
 
-var total= ((trapezoid[base1])+(trapezoid[base2])/ 2 * 8); // this is the formula to find the area of a trapezoid
+var total= ((trapezoid[base1]) + (trapezoid[base2])/2 * (trapezoid[height]) ); // this is the formula to find the area of a trapezoid, I also put my arrays in there too
 
-console.log("For the first base you chose the number "+base1+ ", for the second base you chose the number " +base2+ ", and for the height you picked " +height+ " when these numbers are input in the formula formula for the area of a trapezoid, the answer is " +total);
+console.log("For the first base you chose the number "+base1+ ", for the second base you chose the number " +base2+ ", and for the height you picked " +height+ ", when these numbers are input in the formula for the area of a trapezoid, the answer is " +total);
 
