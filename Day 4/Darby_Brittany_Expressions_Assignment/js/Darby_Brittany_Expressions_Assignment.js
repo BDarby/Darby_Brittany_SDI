840da971977354c0b94656ab69c73a9c041a06e6
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-15 18:43:49 -0500

    Created Day 5 conditional and other things
     created it in web storm

--- Day 4/Darby_Brittany_Expressions_Assignment/js/Darby_Brittany_Expressions_Assignment.js
+++ Day 4/Darby_Brittany_Expressions_Assignment/js/Darby_Brittany_Expressions_Assignment.js
@@ -3,7 +3,7 @@ Brittany Darby
 SDI sec.1 C201501
 1/13/2015
  */
-var alert=("Testing to see if this works"); //this is the alert to see if everything is working
+ //alert=("Testing to see if this works"); //this is the alert to see if everything is working
 
 
 // Finding the Area of a Trapezoid
@@ -12,11 +12,14 @@ var alert=("Testing to see if this works"); //this is the alert to see if everyt
 var base1= parseInt(prompt("Please enter a number for the first base of the Trapezoid"));
 var base2= parseInt(prompt("Please enter a number for the second base of the trapezoid"));
 var height= parseInt(prompt("Please enter a number for the height"));
+//these are my variables
+
 
 var trapezoid= [base1,base2,height];//this is my array for the trapezoid
 
 
-var total= ((trapezoid[base1])+(trapezoid[base2])/ 2 * 8); // this is the formula to find the area of a trapezoid
+var total=((trapezoid[0]+trapezoid[1]) / 2) * trapezoid[2]; // this is the formula to find the area of a trapezoid, I also put my arrays in there too
 
-console.log("For the first base you chose the number "+base1+ ", for the second base you chose the number " +base2+ ", and for the height you picked " +height+ " when these numbers are input in the formula formula for the area of a trapezoid, the answer is " +total);
 
+console.log("For the first base you chose the number "+base1+ ", for the second base you chose the number " +base2+ ", and for the height you picked " +height+ ", when these numbers are input in the formula for the area of a trapezoid, the answer is " +total);
+// above is my concatenation and the answer
---
+++ Day 5/js/Day5Conditional.js
@@ -0,0 +1,10 @@
+
+ /*
+Brittany Darby
+SDI Campus
+1/15/2015
+Conditionals
+ */
+//alert("Testing to see if it work");
+
+ 
