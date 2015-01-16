2470c46355b922da3c2922d2e8512e11f124bb6f
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-15 22:59:10 -0500

    Multiple Results
    I have finished multiple results:grade

--- Conditional_Worksheet/js/Conditional_worksheet.js
+++ Conditional_Worksheet/js/Conditional_worksheet.js
@@ -23,4 +23,20 @@ Prompting
 
  //Multiple Results
 
+var grade= (parseInt(prompt("Enter the number of what grade you got")));
+
+if(grade>90){
+ console.log("You have a " +grade+ " which means you earned an A");
+
+}else if(grade>=80){
+ console.log("You have a " +grade+ " which means you earned a B");
+
+}else if(grade>= 70){
+ console.log("You have a " +grade+ " which means you earned a C");
+} else {
+  console.log("You have a " +grade+ " which means you get a F");
+ }
+
+
+ //Multiple Conditions
 
