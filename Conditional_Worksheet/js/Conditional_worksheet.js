cd7155e9ad860098a64c9271e3437b903846782a
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-15 22:40:24 -0500

    first part of my conditional assignment
    Expressions with conditionals

---
+++ Conditional_Worksheet/js/Conditional_worksheet.js
@@ -0,0 +1,26 @@
+
+ /*
+Brittany Darby
+SDI Campus
+1/13/2015
+Prompting
+ */
+//alert("Testing to see if it work");
+
+//Expressions with Conditionals
+
+ var gasMileage= 25;
+ var carCapacity= 35;
+
+
+ if(carCapacity>gasMileage){
+  console.log("Yes you can make it without stopping");
+
+ }else{
+  console.log("You only have " +carCapacity+ " gallons in your tank, better get gas while you can");
+ }
+
+
+ //Multiple Results
+
+
