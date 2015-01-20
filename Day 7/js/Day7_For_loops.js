9655710f77b3f88bc8d45f15b9091a9068ac6c6a
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-20 18:50:38 -0500

    learned about for loops
    and break command

--- Day 7/js/Day7_For_loops.js
+++ Day 7/js/Day7_For_loops.js
@@ -2,6 +2,38 @@
  *Brittany Darby
  *1/20/2015
  *For loops
+ */
 
 
-//alert("Teseting");
+
+//alert("Testing");
+
+
+//basic for loop structure
+
+
+
+ //for(initialization: conditon to test; increment of change){
+//
+// 
+//Code to run as long as the condititon is true
+//
+//}
+
+
+for (var i=0; i<20; i+=4) {
+    //code to run as long as i<20
+    console.log("The value of i= "+i);
+}
+
+
+for (var k=0; k<5; k++) {
+    console.log("The value for k is "+k);
+    //add a break point
+    //if k=3 then stop the code
+    
+    if (k===3) {
+        
+        break;
+    }
+}
