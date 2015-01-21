738f7aa5baa99448578069feba682b62e675a2a3
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-21 16:26:43 -0500

    I don't know how to validate
    i need help with validation.

--- loops_worksheet/js/loops_worksheet.js
+++ loops_worksheet/js/loops_worksheet.js
@@ -8,6 +8,7 @@
 //alert("testing");
 
 //This is my Do while loop
+
 var days=prompt("Please enter the day you would like to start at, 1-31")
 
 do{
@@ -19,35 +20,41 @@ do{
     days++;
 } while (days<=31);
 
-
+//the other teacher, vik?, helped with this but does not work
 
 
 
 //This is my while loop
 
 var scoops=prompt("Enter the number of scoops you would like 1-4");
-while (scoops<4) {
+
+while (scoops<=4) {
 do{
-    while (isNaN("scoops")){ 
+   while (isNaN("scoops") && age===""){ 
         scoops=prompt("PLEASE ENTER A NUMBER 1-4");
+       scoops++;  //this is the counter
     }
-    console.log("You have "+scoops+" scoops of ice cream")
-    scoops++;
-    break;
+    console.log("You have "+scoops+" scoops of ice cream");
+    scoops++;//do I need 2 counters?
+    
 }while (days<=4); 
 
-
+//^^keeps getting stuck in infinite loop. fml
 
 
 
 //This is my for loop
 dog=prompt("Enter a number of dog(s) you have, 1-7")
+
 for(var dog=1; dog<7; dog++){
+
 console.log("You have "+(dog+1)+" Dog(S)"); //how to add plus one?
-while(dog==="") {
+
+while (isNaN("dogs")){
     dog=prompt("Please enter a number 1-7");
-    dog++;
+    dog++; //this is the counter
 }
 break;
 }
 }
+//^^sometimes errors come up with that last bracket idk why.
