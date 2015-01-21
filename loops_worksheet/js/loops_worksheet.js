3da13dd8f25bb2b23b70685dfd86ad1e0f121d8f
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-20 22:31:36 -0500

    Finish my do while loop
    did the do while loop

--- loops_worksheet/js/loops_worksheet.js
+++ loops_worksheet/js/loops_worksheet.js
@@ -1,1 +1,20 @@
-alert("testing");
+/*
+ *Brittany Darby
+ *1/20/2015
+ *Loops worksheet
+ */
+
+
+//alert("testing");
+
+
+var days=0;
+
+do{
+   
+    days++;
+    if (days===31) {
+        break;
+    }
+    console.log("There are "+days+ " days");
+} while (days<31);
