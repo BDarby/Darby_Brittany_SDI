f3532a45a1af64b7f25dc88d51be6f91cdbfa1bf
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-20 20:43:55 -0500

    learned about validating prompts while loops
    validating

---
+++ Day 7/js/Day7_validating_Prompts.js
@@ -0,0 +1,81 @@
+/*
+ *Brittany Darby
+ *1/20/2015
+ *Validating Prompts
+ */
+
+//alert("testing");
+
+
+var userInput=prompt("Please type in your name:");
+
+//validating prompt with an conditional
+if (userInput===" ") {
+    //user did not type anything in
+    userInput=prompt ("Please don not leave it blank");
+}
+
+//insted use a while loop
+//check the input multiple times as many as it takes
+
+var firstName= prompt("Please type in your first name:");
+
+while(firstName===" "){
+    //this means the user typed in nothing
+    //reprompt for the first time
+    firstName= prompt("please type in your first name and DON'T LEAVE IT BLANK ");
+    
+}
+
+
+//to test to see if it is a number
+//isNan()- test what is inside of (). It is not a number?
+//numbers return false
+//everything else returns true
+
+console.log(isNaN("Monkey")); //returns true
+console.log(isNaN(7));//returns false
+
+//let's ask the user their age
+
+var age=prompt("please enter your age");
+
+//now validate if the user typed in a number
+while(isNaN(age) || age ===""){
+    //this codes runs anytime the age is not a number.
+    //reprompt the user for a number
+    age=prompt("only use numbers, please enter your age");
+}
+
+
+//convert to lower case
+choice= choice.toLowerCase();
+
+var choice= prompt("Please type in yes or no");
+
+while (choice!="yes" && choice!="no") {
+    choice= prompt("please type in yes or no")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
