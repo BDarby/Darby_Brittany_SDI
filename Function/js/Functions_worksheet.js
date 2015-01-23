ad4c872550b1f7a0b1b004b62f7c7e2b2622de11
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-22 23:28:13 -0500

    Finished functions ws(surprisingly)
    I have finished the functions ws.

--- Function/js/Functions_worksheet.js
+++ Function/js/Functions_worksheet.js
@@ -9,20 +9,29 @@
 
 //This is circumference
 
+var radius = 2
+
 function circleCircumference(r) {
 
     var total = 2*Math.PI*r;  //I used the math function
     return total;
 }
- var returntotal= circleCircumference(4);
+ var returntotal= circleCircumference(radius);
 console.log("The circumference of the cirlce is "+returntotal.toFixed(1));
 
 
 //Stung!
-function beeStings() {
-    
+
+    var weight= 192;
+    var toKill=8.666;
+
+function beeStungs(w, tK) {
+    var beeStings = w*tK;
+    return beeStings;
 }
 
+var returnBeeStings = beeStungs(weight, toKill);
 
+console.log("It takes "+returnBeeStings.toFixed(1)+" bee stings to kill a human.");
 
 
