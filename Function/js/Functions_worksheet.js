df87b6c9b53c6e88abdc8b77c7f0052e626ced41
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-22 22:59:44 -0500

    functions ws
    i have done the first part

--- Function/js/Functions_worksheet.js
+++ Function/js/Functions_worksheet.js
@@ -4,4 +4,25 @@
  *Functions Worksheet
  */
 
-alert("testing");
+//alert("testing");
+
+
+//This is circumference
+
+function circleCircumference(r) {
+
+    var total = 2*Math.PI*r;  //I used the math function
+    return total;
+}
+ var returntotal= circleCircumference(4);
+console.log("The circumference of the cirlce is "+returntotal.toFixed(1));
+
+
+//Stung!
+function beeStings() {
+    
+}
+
+
+
+
