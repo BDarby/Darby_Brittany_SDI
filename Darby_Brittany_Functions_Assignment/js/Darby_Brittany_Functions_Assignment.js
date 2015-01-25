30a1bb90eba0d3f02ece13ae78f3af63b73dea35
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-24 21:16:53 -0500

    Functions assignment
    just finished finding the variables and the prompts

--- Darby_Brittany_Functions_Assignment/js/Darby_Brittany_Functions_Assignment.js
+++ Darby_Brittany_Functions_Assignment/js/Darby_Brittany_Functions_Assignment.js
@@ -10,4 +10,17 @@ var width=prompt("Please enter a number for the width");
 while (width==="" || isNaN(width)) {
     width=prompt("Please re enter a number for width");
 }
-console.log(width);
+//console.log(width);
+
+
+var length=prompt("Please enter a number for the length");
+while(length==="" || isNaN(length)){
+    length=prompt("Please re enter a number for the length");
+}
+
+var height=prompt("Please enter a number for the height");
+while (height==="" || isNaN(height)) {
+    length=prompt("Please re enter a number for height")
+}
+
+
