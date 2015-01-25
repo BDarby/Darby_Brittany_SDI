92e7f0bbe0031797d394dc912f73f7f6d5f794f0
BDarby <b_nicole@Brittanys-MacBook-Pro.local>
2015-01-24 21:12:28 -0500

    created  function folder
    assignment function folder

---
+++ Darby_Brittany_Functions_Assignment/js/Darby_Brittany_Functions_Assignment.js
@@ -0,0 +1,13 @@
+/*
+ *Brittany Darby
+ *1-24-2015
+ *Functions Assignment
+ */
+
+//alert("testing");
+
+var width=prompt("Please enter a number for the width");
+while (width==="" || isNaN(width)) {
+    width=prompt("Please re enter a number for width");
+}
+console.log(width);
